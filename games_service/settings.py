74339e29fd5816a53a04abbc581ce55a37eacf4c
Daniel Ale <danielale9291@gmail.com>
2019-08-06 10:06:58 +0100

    Create a game model and make migrations

--- games_service/settings.py
+++ games_service/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'games.app.GamesConfig',
+    'games',
     'rest_framework'
 ]
 
